c281ab42eae0e18f27e62879842a5b0406e0eed2
AndersonCodhugo <anderson_hindo@hotmail.com>
2025-10-14 20:11:27 -0300

    desbugando

--- wppbotmanager/src/main/java/com/wpp/wppbotmanager/dto/RelatorioDetalhadoDTO.java
+++ wppbotmanager/src/main/java/com/wpp/wppbotmanager/dto/RelatorioDetalhadoDTO.java
@@ -1,4 +1,18 @@
 package com.wpp.wppbotmanager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
 public class RelatorioDetalhadoDTO {
+    private String codigo_filtrado;
+    private Periodo periodo_analisado;
+    private String valor_total;
+    private int numero_de_lancamentos;
+    private List<Lancamento> lancamentos;
 }
+
--- wppbotmanager/src/main/java/com/wpp/wppbotmanager/service/PDFGeneratorService.java
+++ wppbotmanager/src/main/java/com/wpp/wppbotmanager/service/PDFGeneratorService.java
@@ -1,43 +1,61 @@
 package com.wpp.wppbotmanager.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+import com.wpp.wppbotmanager.dto.Lancamento;
+import com.wpp.wppbotmanager.dto.RelatorioDetalhadoDTO;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @Service
 public class PDFGeneratorService {
 
     public void export(HttpServletResponse response) throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream("/detalhado.json");
+        if (inputStream == null) {
+            throw new FileNotFoundException("Arquivo detalhado.json não encontrado no classpath!");
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        RelatorioDetalhadoDTO relatorio = mapper.readValue(inputStream, RelatorioDetalhadoDTO.class);
+
         Document documento = new Document();
-
         PdfWriter.getInstance(documento, response.getOutputStream());
-
         documento.open();
-        Font tituloFonte = FontFactory.getFont(FontFactory.TIMES_ROMAN);
-        tituloFonte.setSize(24);
+        try {
+            Image img = Image.getInstance(getClass().getResource("/logo.png"));
+            img.setAlignment(Image.ALIGN_CENTER);
+            img.scaleToFit(100, 100);
+            documento.add(img);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        Font conteudo = FontFactory.getFont(FontFactory.HELVETICA);
+        NumberFormat formatadorMoeda = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 
-        Paragraph titulo = new Paragraph("RELATÓRIO", tituloFonte);
-        titulo.setAlignment(Paragraph.ALIGN_CENTER);
-
-        PdfPTable tabela = new PdfPTable(5);
-        PdfPCell cell;
-        cell = new PdfPCell(new Phrase("Lançamentos:", conteudo));
-        cell.setRowspan(5);
-        tabela.addCell(cell);
-        tabela.addCell("R$ 0,00");
-        tabela.addCell("R$ 0,00");
-        tabela.addCell("R$ 0,00");
-        tabela.addCell("R$ 0,00");
-
-        documento.add(titulo);
+        PdfPTable tabela = new PdfPTable(3);
+        tabela.addCell("Data");
+        tabela.addCell("Descrição");
+        tabela.addCell("Valor");
+        for (Lancamento l : relatorio.getLancamentos()) {
+            tabela.addCell(l.getData());
+            tabela.addCell(l.getDescricao());
+            String valorFormatado = formatadorMoeda.format(l.getValor());
+            tabela.addCell(valorFormatado);
+        }
         documento.add(tabela);
+
+
         documento.close();
     }
 }
