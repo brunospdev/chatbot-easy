449ee6edf8fcb44044cb579cce209cdc0c09bfab
Anderson Hugo Santos Silva Martins <1242115614@acd.grupotiradentes.com>
2025-11-05 20:48:03 -0300

    fix/alterações para funcionamento global

--- wppbotmanager/src/main/java/com/wpp/wppbotmanager/dto/PeriodoAnalisadoDTO.java
+++ wppbotmanager/src/main/java/com/wpp/wppbotmanager/dto/PeriodoAnalisadoDTO.java
@@ -1,12 +1,20 @@
 package com.wpp.wppbotmanager.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PeriodoAnalisadoDTO {
+
+    @JsonProperty("dataInicio")
     private String data_inicio;
+
+    @JsonProperty("dataFim")
     private String data_fim;
+
+    @JsonProperty("dataDias")
     private Integer total_dias;
+
 }
